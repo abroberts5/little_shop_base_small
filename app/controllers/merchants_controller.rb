f37fbc0b4b3b63068f1a9273d4946853add51388
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2019-01-08 11:13:11 -0700

    Created and added method for top 10 merch products in model and in controller to call in view.

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -14,6 +14,8 @@ class MerchantsController < ApplicationController
     @top_3_states = Order.top_3_states
     @top_3_cities = Order.top_3_cities
     @top_3_quantity_orders = Order.top_3_quantity_orders
+    @top_10_merch_this_month = User.top_10_merch_this_month(10)
+    @top_10_merch_last_month = User.top_10_merch_last_month(10)
   end
 
   def show
