fa82e676eea1d1d58ac2eb8c6149751e179cfb01
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2019-01-08 19:57:01 -0700

    updated merchant controller with if statement, was changing current user and conflicting with other tests

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -18,6 +18,12 @@ class MerchantsController < ApplicationController
     @top_10_merch_last_month = User.top_10_merch_last_month(10)
     @top_merch_orders_this_month = User.top_merch_orders_this_month(10)
     @top_merch_orders_last_month = User.top_merch_orders_last_month(10)
+
+    if current_user
+      user = current_user
+      @top_merch_for_state = User.top_merch_in_state(user)
+      @top_merch_for_city = User.top_merch_in_city(user)
+    end
   end
 
   def show
